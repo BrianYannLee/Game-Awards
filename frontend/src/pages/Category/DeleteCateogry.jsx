37a567eb0d0d67ef31827073168b21de51769b63
BrianYannLee <165946084+BrianYannLee@users.noreply.github.com>
2025-12-02 10:29:47 -0800

    frontend begin

---
+++ frontend/src/pages/Category/DeleteCateogry.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DeleteCateogry() {
+  return <div>DeleteCateogry</div>;
+}
---
+++ frontend/src/pages/Game/EditGame.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function EditGame() {
+  return <div>EditGame</div>;
+}
